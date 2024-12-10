3007a790e7add78f9e5212b1af7d7be18a92cf2b
YashLonkar <yashlonkar99@gmail.com>
2024-12-10 13:16:27 +0530

    Updated app deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  plugins: "/vite-deploy",
+  base: "/My-Personal-Portfolio",
 
   server: {
     host: true, // Allows access from external devices
