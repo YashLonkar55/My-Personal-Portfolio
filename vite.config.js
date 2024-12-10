dea24b759f75488d36dbbfc86174fd0d58e726ce
YashLonkar <yashlonkar99@gmail.com>
2024-12-10 13:07:22 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  plugins: "/vite-deploy",
 
   server: {
     host: true, // Allows access from external devices
